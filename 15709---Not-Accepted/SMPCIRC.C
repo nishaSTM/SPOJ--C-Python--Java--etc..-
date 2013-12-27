1a9d836f1cb11db8bf9d4e004e2a52e41ce7edb6
KKSV ' chasing zeroes ' <chicagobostondeveloper@gmail.com>
2013-12-26 19:30:02 -0500

    Create SMPCIRC.C

---
+++ 15709---Not-Accepted/SMPCIRC.C
@@ -0,0 +1,29 @@
+/*
+
+15709. Two Circles
+Problem code: SMPCIRC
+
+Given two circles: O1 with the center o1 =(xo1, yo1) and a radius r1 and O2 with the center o2 =(xo2, yo2) and radius r2, please compute if O1 is inside O2.
+Input description
+
+First t < 1000, the number of test cases. In each of the following t lines, 6 integers: xo1 yo1 r1 xo2 yo2 r2. Where 0 ≤ xo1, yo1, xo2, yo2 ≤ 1000 and 0 < r1, r2 ≤ 1000.
+Output description
+
+For each test case print one character:
+I, if O1 is inside O2,
+E, if O1 is internally tangent to O2,
+O, in other cases.
+Example
+
+Input:
+2
+103 104 5 100 100 10
+103 104 10 100 100 10
+
+Output:
+E
+O
+
+
+*/
+
