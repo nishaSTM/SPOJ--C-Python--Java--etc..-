3ebac95a36af915cd366b35aedbff8b13c3360cc
KKSV ' chasing zeroes ' <chicagobostondeveloper@gmail.com>
2014-01-05 08:46:47 -0500

    Update NSYSTEM.py

--- 1669/NSYSTEM.py
+++ 1669/NSYSTEM.py
@@ -2,20 +2,26 @@ def r1(a):
  i = 0
  multiplier = 1
  for _a in a:
+  
   if _a == 'm':
    i = i + ( multiplier * 1000)
    multiplier = 1
+  
   if _a == 'c':
    i = i + ( multiplier * 100)
    multiplier = 1
+
   if _a == 'x':
    i = i + ( multiplier * 10)
    multiplier = 1
+   
   if _a == 'i':
    i = i + multiplier
    multiplier = 1
+  
   if ((_a >= '2') and (_a <= '9')):
    multiplier = (ord(_a)-ord('0'))
+ 
  return i
  
 def r2(n):
