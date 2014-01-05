0fcfeb82ad3e55fdb286ee51d808d307ce31969e
KKSV ' chasing zeroes ' <chicagobostondeveloper@gmail.com>
2014-01-05 06:57:16 -0500

    Update NSYSTEM.py

--- 1669/NSYSTEM.py
+++ 1669/NSYSTEM.py
@@ -17,14 +17,40 @@ def r1(a):
   if ((_a >= '2') and (_a <= '9')):
    multiplier = (ord(_a)-ord('0'))
  return i
-
-print r1('xi')+r1('x9i')
-print r1('i')+r1('9i')
-print r1('c2x2i')+r1('4c8x8i')
-print r1('m2ci')+r1('4m7c9x8i')
-print r1('9c9x9i')+r1('i')
-print r1('i')+r1('9m9c9x8i')
-print r1('m')+r1('i')
-print r1('i')+r1('m')
-print r1('m9i')+r1('i')
-print r1('9m8c7xi')+r1('c2x8i')
+ 
+def r2(n):
+ 	result = ''
+ 	i = n/1000
+ 	if (i):
+ 		result+=str(i)
+ 		result+='m'
+ 	n = n % 1000
+ 	
+ 	i = n/100
+ 	if (i):
+ 		result+=str(i)
+ 		result+='c'
+ 	n = n % 100
+ 	
+ 	i = n/10
+ 	if (i):
+ 		result+=str(i)
+ 		result+='x'
+ 	n = n % 10
+ 	
+ 	i = n
+ 	if (i):
+ 		result+=str(i)
+ 		result+='i'
+ 	return result
+ 
+print r2(r1('xi')+r1('x9i'))
+print r2(r1('i')+r1('9i'))
+print r2(r1('c2x2i')+r1('4c8x8i'))
+print r2(r1('m2ci')+r1('4m7c9x8i'))
+print r2(r1('9c9x9i')+r1('i'))
+print r2(r1('i')+r1('9m9c9x8i'))
+print r2(r1('m')+r1('i'))
+print r2(r1('i')+r1('m'))
+print r2(r1('m9i')+r1('i'))
+print r2(r1('9m8c7xi')+r1('c2x8i'))
