24c78b931d7b7220b243428a6087b60bd35c3eea
KKSV ' chasing zeroes ' <ChicagoBostonDeveloper@gmail.com>
2014-01-13 10:33:11 -0500

    Update HPYNOS.py

--- 7733/HPYNOS.py
+++ 7733/HPYNOS.py
@@ -1,41 +1,40 @@
-The process of “breaking” an integer is defined as summing the squares of its digits. For example, the
-result of breaking the integer 125 is (12 + 22 + 52) = 30. An integer N is happy if after “breaking” it
-repeatedly the result reaches 1. If the result never reaches 1 no matter how many times the
-“breaking” is repeated, then N is not a happy number.
+# The process of “breaking” an integer is defined as summing the squares of its digits. For example, the
+# result of breaking the integer 125 is (12 + 22 + 52) = 30. An integer N is happy if after “breaking” it
+# repeatedly the result reaches 1. If the result never reaches 1 no matter how many times the
+# “breaking” is repeated, then N is not a happy number.
 
-TASK
+# TASK
 
-Write a program that given an integer N, determines whether it is a happy number or not.
+# Write a program that given an integer N, determines whether it is a happy number or not.
 
-CONSTRAINTS
+# CONSTRAINTS
 
-2 ≤ N ≤ 2,147,483,647
+# 2 ≤ N ≤ 2,147,483,647
 
-Input
+# Input
 
-    • A single line containing a single integer N.
+#     • A single line containing a single integer N.
 
-Output
+# Output
 
-    • A single line containing a single integer T which is the number of times the process had to be
-      done to determine that N is happy, or -1 if N is not happy.
+#     • A single line containing a single integer T which is the number of times the process had to be
+#       done to determine that N is happy, or -1 if N is not happy.
 
-Example
+# Example
 
-Input:
-19
+# Input:
+# 19
 
-Output:
-4
+# Output:
+# 4
 
-
-1) 19   : 12 + 92 = 82
-2) 82   : 82 + 22 = 68
-3) 68   : 62 + 82 = 100
-4) 100 : 12 + 02 + 02 = 1
+# 1) 19   : 12 + 92 = 82
+# 2) 82   : 82 + 22 = 68
+# 3) 68   : 62 + 82 = 100
+# 4) 100 : 12 + 02 + 02 = 1
 
 # The solution is 4 because we discovered that the integer 19 is happy after we repeated the process 4
-times.
+# times.
 
 
 # Example
