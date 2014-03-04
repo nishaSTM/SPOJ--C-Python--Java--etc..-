481ad19e5b2915c9169ce80c83d9b3739145f73c
KKSV ' chasing zeroes ' <ChicagoBostonDeveloper@gmail.com>
2014-03-04 10:59:18 -0500

    Update KAMIL.py

--- 53/KAMIL.py
+++ 53/KAMIL.py
@@ -1,8 +1,14 @@
-#Some kids cannot pronounce all letters, some of them they sometimes pronounce correctly and sometimes incorrectly. Kamil sometimes says T instead of K, but he never says K instead of T. Similarly he sometimes says D instead of G. Instead of R he sometimes says L and sometimes F. Of course it happens that he pronounces the letter correctly. Kamil's father always thinks how many words can mean the word spoken by his son (it doesn't matter if they are real English words).
+# 53. Kamil
+#
+# Some kids cannot pronounce all letters, some of them they sometimes pronounce correctly and sometimes incorrectly. 
+# Kamil sometimes says T instead of K, but he never says K instead of T. Similarly he sometimes says D instead of G.
+# Instead of R he sometimes says L and sometimes F. Of course it happens that he pronounces the letter correctly.
+# Kamil's father always thinks how many words can mean the word spoken by his son (it doesn't matter if they are real 
+#                                                                                                     English words).
 
-#Task
+# Task
 
-#Write a program which
+# Write a program which
 
 #    reads from standard input the words spoken by Kamil
 #    counts how many different words can that mean
